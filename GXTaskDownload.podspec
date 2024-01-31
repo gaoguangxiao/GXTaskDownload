2f6b07d68338335730d4813f08cce29401747b2d
gaoguangxiao <gaoguangxiao125@sina.com>
2024-01-31 11:51:15 +0800

    优化多任务下载

--- GXTaskDownload.podspec
+++ GXTaskDownload.podspec
@@ -8,8 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = 'GXTaskDownload'
-  s.version          = '0.2.3'
-  s.summary          = '暴漏磁盘外部访问'
+  s.version          = '0.2.4'
+  s.summary          = '优化多任务下载'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
--- GXTaskDownload/Classes/GXDownloadManager.swift
+++ GXTaskDownload/Classes/GXDownloadManager.swift
@@ -12,6 +12,9 @@ public class GXDownloadManager: NSObject {
     /// URLS总数量
     private var tasksCount: Float = 0
     
+    /// 已经完成的任务数量
+    private var finishTasksCount: Float = 0
+    
     private let executeQueue = DispatchQueue(label:"asyncQueue",
                                              qos: .default,
                                              attributes: .concurrent,
@@ -21,31 +24,21 @@ public class GXDownloadManager: NSObject {
     /// 最大下载数
     public var maxConcurrentCount = 9
     
-    /// 等待的任务
+    /// 需要下载的任务
+    private var waitingUrlsTasks: Array<GXTaskDownloadDisk> = []
+    
+    /// 等待下载的任务
     private var waitingTasks: Array<GXTaskDownloadDisk> = []
+    
+    /// 正在下载的任务
+    private var downloadingTasks: Array<GXTaskDownloadDisk> = []
         
     private var downloadTotalBlock: GXTaskDownloadTotalBlock?
         
-    func progressCallBack() {
-//        print("等待任务数量:\(waitTaskcount)")
-        let downCounted = self.tasksCount - Float(waitTaskcount)
-//        print("进度:\(progress)")
-        objc_sync_enter(self)
-        if let pro = self.downloadTotalBlock {
-            pro(self.tasksCount,downCounted,.downloading)
-        }
-        objc_sync_exit(self)
-        //        回调进度还需要优化--
-        //        objc_sync_enter(self)
-        //        if let pro = self.downloadBlock {
-        //            pro(progress, .downloading)
-        //        }
-        //        objc_sync_exit(self)
-    }
-    
+
     func stateCallBack(state: GXDownloadingState) {
 //        print("等待任务数量:\(waitTaskcount)")
-        let downCounted = self.tasksCount - Float(waitTaskcount)
+        let downCounted = self.finishTasksCount
 //        print("进度:\(progress)")
         objc_sync_enter(self)
 //        if let pro = self.downloadBlock {
@@ -57,8 +50,13 @@ public class GXDownloadManager: NSObject {
         objc_sync_exit(self)
     }
     
+    func getOfflineManifestFolder(url: String) -> String {
+        let folderName = url.lastPathComponent.removeMD5.stringByDeletingPathExtension
+        return folderName
+    }
+    
     deinit {
-//        print("\(self)-deinit")
+        print("\(self)-deinit")
     }
 }
 
@@ -76,6 +74,7 @@ extension GXDownloadManager {
         return taskCount
     }
     
+    
     func enqueue(urlModel: GXDownloadURLModel, path: String) {
         /// 创建一个任务
         let downloader = GXTaskDownloadDisk()
@@ -84,41 +83,99 @@ extension GXDownloadManager {
         waitingTasks.append(downloader)
     }
     
-    func execute()  {
-        let downloader = gainTask()
-        downloader?.start(block: { [weak self] progress, state in
-            guard let `self` = self else { return }
-            //单个文件的下载完成，抛出进度
-            if state == .completed || state == .error {
-                self.removeTask()
-                //判断是否存在未执行的任务
-                if self.waitTaskcount == 0 {
-//                    print("没有任务可供下载")
-                    self.stateCallBack(state: .completed)
-                } else {
-                    self.execute()
-                    //进度回调
-                    self.stateCallBack(state: .downloading)
+    
+    /// 以URL后缀存储某个文件，不拼接url的路径
+    /// - Parameters:
+    ///   - urlModel: <#urlModel description#>
+    ///   - path: <#path description#>
+    func enqueueByPath(path: String, urlModel: GXDownloadURLModel) {
+        /// 创建一个任务
+        let downloader = GXTaskDownloadDisk()
+        //下载新配置 获取URL可匹配
+        if let url = urlModel.src {
+            let folderName = url.lastPathComponent.removeMD5.stringByDeletingPathExtension
+            downloader.diskFile.taskDownloadPath = path.stringByAppendingPathComponent(path: folderName)
+        } else {
+            downloader.diskFile.taskDownloadPath = path
+        }
+        downloader.prepare(urlModel: urlModel)
+        waitingTasks.append(downloader)
+    }
+    
+    func execute() {
+        if let downloader = gainTask() {
+            self.removeTask()
+            self.addDownloadTask(task: downloader)
+            downloader.start(block: { [weak self] progress, state in
+//                LogInfo("weakself开始下载")
+                guard let `self` = self else { return }
+                //单个文件的下载完成，抛出进度
+                if state == .completed || state == .error {
+                    self.removeDownloadTask(task: downloader)
+                    self.addFinishTaskCount()                    
                 }
-            }
-        })
+            })
+        }
     }
     
     
     /// 获取可执行的第一个任务
     /// - Returns: <#description#>
     func gainTask() -> GXTaskDownloadDisk? {
+        guard self.waitingTasks.count != 0 else {
+            return nil
+        }
         let downloader = waitingTasks.first
         return downloader
+    }
+
+    func addFinishTaskCount() {
+        objc_sync_enter(self)
+        self.finishTasksCount += 1
+        //判断是否存在未执行的任务
+//        print("完成任务数量:\(self.finishTasksCount)")
+//        print("总任务数量:\(self.tasksCount)")
+//        print("等待任务数量:\(self.waitTaskcount)")
+//        print("正在下载任务数量:\(self.downloadingTasks.count)")
+//        
+        if self.finishTasksCount == self.tasksCount {
+            self.stateCallBack(state: .completed)
+        } else {
+            self.execute()
+            //进度回调
+            self.stateCallBack(state: .downloading)
+        }
+        objc_sync_exit(self)
     }
     
     /// 移除任务
     func removeTask() {
+        objc_sync_enter(self)
         guard self.waitingTasks.count != 0 else {
             return
         }
-        objc_sync_enter(self)
         self.waitingTasks.removeFirst()
+        objc_sync_exit(self)
+    }
+    
+    /// 添加正在下载的任务
+    /// - Returns: description
+    func addDownloadTask(task: GXTaskDownloadDisk) {
+        objc_sync_enter(self)
+        self.downloadingTasks.append(task)
+        objc_sync_exit(self)
+        
+    }
+    
+    /// 移除正在下载的任务
+    func removeDownloadTask(task: GXTaskDownloadDisk) {
+        guard self.downloadingTasks.count != 0 else {
+            return
+        }
+        objc_sync_enter(self)
+        self.downloadingTasks.removeAll { _task in
+            return _task == task
+        }
         objc_sync_exit(self)
     }
 }
@@ -143,6 +200,36 @@ extension GXDownloadManager {
         //入队
         for url in urls {
             self.enqueue(urlModel: url, path: path)
+        }
+        
+        tasksCount = Float(urls.count)
+        
+        //执行
+        self.executeQueue.async {
+            for _ in 0 ..< self.maxConcurrentCount {
+                //                print("下载次数：\(1)")
+                self.execute()
+            }
+        }
+    }
+    
+    /// 下载一组URLS
+    /// - Parameters:
+    ///   - urls: urls description
+    ///   - path: <#path description#>
+    ///   - block: <#block description#>
+    public func startByURL(forURL urls: Array<GXDownloadURLModel>,
+                      maxDownloadCount: Int = 9,
+                      path: String,
+                      block: @escaping GXTaskDownloadTotalBlock) {
+    
+        self.maxConcurrentCount = maxDownloadCount
+        self.downloadTotalBlock = block
+        LogInfo("startByURL开始下载")
+        
+        //入队
+        for url in urls {
+            enqueueByPath(path: path, urlModel: url)
         }
         
         tasksCount = Float(urls.count)
